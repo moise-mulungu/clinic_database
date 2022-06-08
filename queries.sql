4b26312676db442271ff34537b3247aa5a33487f
moise <moisemlg90@gmail.com>
2022-06-08 15:56:16 +0200

    Create a table named patients

--- queries.sql
+++ queries.sql
@@ -3,3 +3,10 @@
 -- create a database named clinic
 CREATE DATABASE clinic;
 
+-- create a table named patients
+CREATE TABLE patients(
+    id INT GENERATED ALWAYS AS IDENTITY, 
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE,
+    PRIMARY KEY (id)
+);
