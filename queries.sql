5e91056d622572120302c87324b8224f8c0a76b5
moise <moisemlg90@gmail.com>
2022-06-08 16:10:21 +0200

    Create a table named medical_histories

--- queries.sql
+++ queries.sql
@@ -10,3 +10,15 @@ CREATE TABLE patients(
     date_of_birth DATE,
     PRIMARY KEY (id)
 );
+
+-- create a table named medical_histories
+CREATE TABLE medical_histories(
+    id INT GENERATED ALWAYS AS IDENTITY, 
+    admitted_at TIMESTAMP,
+    patients_id INT,
+    status VARCHAR(50) NOT NULL,
+    CONSTRAINT fk_patients
+        FOREIGN KEY(patients_id)
+        REFERENCES patients(id)
+);
+
